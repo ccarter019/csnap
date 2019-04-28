d2c268dfbe51ce4e523675c0b574cad575560588
ccarter019 <webmaster@coldsnap.net>
2019-04-28 13:19:55 -0700

    Saving point for jquery form, delete items display deleted items above

--- js/main.js
+++ js/main.js
@@ -8,13 +8,40 @@ let items = {
   comments: "Comments"
 };
 
+let disItems = new Object();
+
+let displayAction = () => {
+  var listBox = $("#deletedItems");
+
+  Object.keys(disItems).forEach(function(key, index) {
+    console.log(key);
+    $("#deletedItems").empty();
+    $("#deletedItems").append(key);
+    $("#deletedItems").appendTo("Hello");
+    console.log(index);
+  });
+};
+
 $(".iconA a").click(function() {
   $(this)
     .parent()
     .parent()
     .hide();
   var cField = $(this).data("ntag");
+  console.log(
+    $(this)
+      .closest("div")
+      .find("label")
+      .html()
+  );
+  var cFieldName = $(this)
+    .closest("div")
+    .find("label")
+    .html();
+  //console.log(cFieldName);
+  disItems[cField] = cFieldName;
   delete items.cField;
+  displayAction();
 });
 
-console.log(items);
+console.log(disItems);
